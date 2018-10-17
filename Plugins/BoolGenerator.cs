e6e25b59ca3fb6f5d727a2962348530d9971c6f2
devvindan <devvindan@gmail.com>
2018-10-17 09:14:12 +0300

    started unit tests

---
+++ Plugins/BoolGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO.DTOGenerator;
+
+
+namespace Plugins
+{
+    public class BoolGenerator : IGenerator, IPluginGenerator
+    {
+        Random generator;
+
+        public BoolGenerator()
+        {
+            generator = new Random();
+        }
+
+        public object Generate()
+        {
+            return generator.Next(1) == 1 ? true : false;
+        }
+
+        public Type GetGeneratorType()
+        {
+            return typeof(bool);
+        }
+
+    }
+}
---
+++ Plugins/IntGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO.DTOGenerator;
+
+namespace Plugins
+{
+    class IntGenerator : IGenerator, IPluginGenerator
+    {
+        Random generator;
+
+        public IntGenerator()
+        {
+            generator = new Random();
+        }
+
+        public object Generate()
+        {
+            return generator.Next(int.MinValue, int.MaxValue);
+        }
+
+        public Type GetGeneratorType()
+        {
+            return typeof(int);
+        }
+    }
+}
